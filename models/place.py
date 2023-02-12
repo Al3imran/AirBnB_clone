6d1fc2cc8a9503f90fa59f0746603b4e9bdecd53
Al3imran <111256161+Al3imran@users.noreply.github.com>
2023-02-12 21:33:21 +0300

    1

--- models/place.py
+++ models/place.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" Class Place """
+"""Module for Place class."""
 
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """ Class Place """
-    name = ""
+    """Class representing a Place."""
     city_id = ""
     user_id = ""
+    name = ""
     description = ""
     number_rooms = 0
     number_bathrooms = 0
-    max_guests = 0
+    max_guest = 0
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
