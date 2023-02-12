00edaa36146510b3440ccc4c4572f283519a409f
Al3imran <111256161+Al3imran@users.noreply.github.com>
2023-02-12 20:48:28 +0300

    1

--- models/state.py
+++ models/state.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python
-""" holds class State"""
+#!/usr/bin/python3
+""" Class State """
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Representation of state """
+    """ Class State """
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
