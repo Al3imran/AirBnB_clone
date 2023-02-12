1c7684468e67f9efb04784c821807f5ed0bb1360
Al3imran <111256161+Al3imran@users.noreply.github.com>
2023-02-12 20:48:53 +0300

    1

--- models/user.py
+++ models/user.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python
-""" holds class User"""
+#!/usr/bin/python3
+""" Class user """
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Representation of a user """
+    """ Class user """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
